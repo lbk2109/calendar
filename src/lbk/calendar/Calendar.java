6896ea9a1ab9b6ffa504c0a39dd4b81e25dceb02
명훈 <@192.168.0.9>
2017-12-16 16:45:21 +0900

    가상의 달력찍기 윤달 고려

--- src/lbk/calendar/Calendar.java
+++ src/lbk/calendar/Calendar.java
@@ -5,23 +5,32 @@ import java.util.Scanner;
 public class Calendar {
 
 	private static final int[] MAXDAY_OF_MONTH = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] MAXDAY_OF_LEAP_MONTH = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int getMaxDayOfMonth(int month) {
-		return (MAXDAY_OF_MONTH[month - 1]);
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+			return true;
+		return false;
+	}
+
+	public int getMaxDayOfMonth(int year, int month) {
+		if (isLeapYear(year))
+		{
+			return (MAXDAY_OF_LEAP_MONTH[month-1]);
+		}
+		return (MAXDAY_OF_MONTH[month-1]);
 	}
 
 	public void prtCalendar(int year, int month) {
-		
-		System.out.printf("<<%4d년%3d월>>\n",year,month);
+
+		System.out.printf("<<%4d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TH WE TH FR SA");
 		System.out.println("---------------------");
-		
-		int maxDayOfMonth = getMaxDayOfMonth(month);
-		for(int i=1; i <= maxDayOfMonth;i++)
-		{
-			System.out.printf("%3d",i);
-			if(i%7 == 0 )
-			{
+
+		int maxDayOfMonth = getMaxDayOfMonth(year,month);
+		for (int i = 1; i <= maxDayOfMonth; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == 0) {
 				System.out.println();
 			}
 		}
@@ -42,7 +51,8 @@ public class Calendar {
 				continue;
 			}
 			Calendar cal = new Calendar();
-			System.out.printf("%d월의 마지막일은 %d입니다\n", month, cal.getMaxDayOfMonth(month));
+//			System.out.printf("%d월의 마지막일은 %d입니다\n", month, cal.getMaxDayOfMonth(2017,month));
+			System.out.printf("%d월의 마지막일은 %d입니다\n", month, cal.getMaxDayOfMonth(2000,month));
 		}
 		System.out.println("Bye~~");
 		scanner.close();
--- src/lbk/calendar/RunPrompt.java
+++ src/lbk/calendar/RunPrompt.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class RunPrompt {
 
-	private static String PROMPT = "Cal> ";
-
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
 		while (true) {
-			System.out.print(PROMPT);
+			System.out.print("Year> ");
+			int year = scanner.nextInt();
+
+			System.out.print("Month> ");
 			int month = scanner.nextInt();
 			if (month < 1) {
 				break;
@@ -19,7 +20,7 @@ public class RunPrompt {
 				continue;
 			}
 			Calendar cal = new Calendar();
-			cal.prtCalendar(2017, month);
+			cal.prtCalendar(year, month);
 		}
 		System.out.println("Bye~~");
 		scanner.close();

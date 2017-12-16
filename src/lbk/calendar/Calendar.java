c361534063f965d9316c2bddb1a686f58505b63c
명훈 <@192.168.0.9>
2017-12-16 14:37:55 +0900

    월을 입력받아 해당월의 마지막일자를 구하기 추가

--- src/lbk/calendar/Calendar.java
+++ src/lbk/calendar/Calendar.java
@@ -1,10 +1,38 @@
 package lbk.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
-	public static void main(String[] args) {
-		System.out.println("칼렌다를 통합 자바학습");
+	private static final int[] MAXDAY_OF_MONTH = {31,29,31,30,31,30,31,31,30,31,30,31};
 
+	public int getMaxDayOfMonth(int month)
+	{
+		return(MAXDAY_OF_MONTH[month-1]);
+	}
+	public void prtSampleCalendar()
+	{
+		System.out.println(" 일  월  화  수  목  금  토");
+		System.out.println("--------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+	
+	}
+	public static void main(String[] args) {
+		
+		//월을 입력받아 해당월의 마지막일자를 구하기
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("월을 입력하세요");
+		int month = scanner.nextInt();
+		scanner.close();
+		
+		Calendar cal = new Calendar();
+		System.out.printf("%d월의 마지막일은 %d입니다",month,cal.getMaxDayOfMonth(month));
+		
+//		샘플칼렌더 출력하기
+//		cal.prtSampleCalendar();
 	}
 
 }

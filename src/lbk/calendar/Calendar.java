2bac36a19a38ea3cc1c3c2a71f44a10b2aae92d6
명훈 <@192.168.0.9>
2017-12-17 17:00:04 +0900

    정교한달력 찍기

--- src/lbk/calendar/Calendar.java
+++ src/lbk/calendar/Calendar.java
@@ -1,7 +1,5 @@
 package lbk.calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
 
 	private static final int[] MAXDAY_OF_MONTH = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
@@ -14,48 +12,43 @@ public class Calendar {
 	}
 
 	public int getMaxDayOfMonth(int year, int month) {
-		if (isLeapYear(year))
-		{
-			return (MAXDAY_OF_LEAP_MONTH[month-1]);
+		if (isLeapYear(year)) {
+			return (MAXDAY_OF_LEAP_MONTH[month - 1]);
 		}
-		return (MAXDAY_OF_MONTH[month-1]);
+		return (MAXDAY_OF_MONTH[month - 1]);
 	}
 
-	public void prtCalendar(int year, int month) {
+	public void prtCalendar(int year, int month, int weekday) {
 
 		System.out.printf("<<%4d년%3d월>>\n", year, month);
-		System.out.println(" SU MO TH WE TH FR SA");
+		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
 
-		int maxDayOfMonth = getMaxDayOfMonth(year,month);
-		for (int i = 1; i <= maxDayOfMonth; i++) {
+		// 공백끼워넣기 " "
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
+
+		int maxDayOfMonth = getMaxDayOfMonth(year, month);
+
+		int count = 7 - weekday;
+		
+		int dellim = (count < 7) ? count : 0;
+		
+		for (int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
-			if (i % 7 == 0) {
+		}
+		System.out.println();
+
+		for (int i = count + 1; i <= maxDayOfMonth; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == dellim) {
 				System.out.println();
 			}
 		}
 		System.out.println();
-	}
+		System.out.println("---------------------");
+		System.out.println();
 
-	public static void main(String[] args) {
-		// 월을 입력받아 해당월의 마지막일자를 구하기
-		String PROMPT = "Cal> ";
-		Scanner scanner = new Scanner(System.in);
-		while (true) {
-			System.out.print(PROMPT);
-			int month = scanner.nextInt();
-			if (month < 1) {
-				break;
-			}
-			if (month > 12) {
-				continue;
-			}
-			Calendar cal = new Calendar();
-//			System.out.printf("%d월의 마지막일은 %d입니다\n", month, cal.getMaxDayOfMonth(2017,month));
-			System.out.printf("%d월의 마지막일은 %d입니다\n", month, cal.getMaxDayOfMonth(2000,month));
-		}
-		System.out.println("Bye~~");
-		scanner.close();
 	}
-
 }
--- src/lbk/calendar/RunPrompt.java
+++ src/lbk/calendar/RunPrompt.java
@@ -4,25 +4,71 @@ import java.util.Scanner;
 
 public class RunPrompt {
 
+	/**
+	 * 
+	 * @param week:요일
+	 *            
+	 * @return : 0~6 :0 일요일,1월요일 .. 6토요일
+	 */
+	public static int parseDay(String week) {
+		if (week.equals("su"))
+			return 0;
+		else if (week.equals("mo"))
+			return 1;
+		else if (week.equals("tu"))
+			return 2;
+		else if (week.equals("we"))
+			return 3;
+		else if (week.equals("th"))
+			return 4;
+		else if (week.equals("fr"))
+			return 5;
+		else if (week.equals("sa"))
+			return 6;
+		else
+			return -1;
+	}
+
 	public static void main(String[] args) {
+
+		int year = 2017;
+		int month = -1;
+		int weekday = 0;
 
 		Scanner scanner = new Scanner(System.in);
 		while (true) {
+			System.out.println("년도를 입력하세요.(-1:종료)");
 			System.out.print("Year> ");
-			int year = scanner.nextInt();
-
-			System.out.print("Month> ");
-			int month = scanner.nextInt();
-			if (month < 1) {
+			year = scanner.nextInt();
+			if (year < 1)
+			{
+				System.out.printf("\n\n\n");
 				break;
 			}
-			if (month > 12) {
+
+			System.out.println("월을 입력하세요.");
+			System.out.print("Month> ");
+			month = scanner.nextInt();
+			if (month > 12 || month < 1)
+			{
+				System.out.printf("잘못된 월의 입력입니다.\n\n\n");
 				continue;
 			}
+
+			System.out.println("첫째낭의 요일을 입력하세요(su mo tu we th fr sa).");
+			System.out.print("WEEK> ");
+			String str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
+			if (weekday < 0)
+			{
+				System.out.print("잘못된 첫째낭의 요일을 입력입니다.\n\n\n");
+				continue;
+			}	
+
 			Calendar cal = new Calendar();
-			cal.prtCalendar(year, month);
+			cal.prtCalendar(year, month, weekday);
 		}
-		System.out.println("Bye~~");
+		System.out.printf("Bye~~\n\n\n");
 		scanner.close();
 
 	}
